3c42866bda8c40635fa9adc350b52bd683a43228
Thefcraft <76678136+thefcraft@users.noreply.github.com>
2023-06-20 11:46:46 +0530

    Create fetch_data_from_civitai.py

---
+++ fetch_data_from_civitai.py
@@ -0,0 +1,14 @@
+import requests
+import json
+
+url = 'https://civitai.com/api/v1/images'
+
+while True:
+    data = requests.get(url)
+    data = json.loads(data.content)
+    url = data['metadata'].get('nextPage')
+    if url == None: break
+
+    items = data['items']
+    # now do whatever you want with data
+
